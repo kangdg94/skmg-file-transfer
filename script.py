e1d71a409f3ff1404e121445af23289073660e88
강동근 <donggeun.kang@sk.com>
2026-06-09 16:17:55 +0900

    test

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
-wss://1r4xiikk8h.execute-api.ap-northeast-2.amazonaws.com/dev/
+{
+  "result": 200, "success": true,
+  "data": {
+    "name": "홍길동",
+    "gender": "Male",
+    "birthDate": "1990-01-01",
+    "loginId": "01012345678",
+    "installer": 0,
+    "passwordChangedAt": "2026-01-01T10:00:00",
+    "passwordExpired": true
+  }
+}
